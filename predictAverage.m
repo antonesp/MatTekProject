c69099604418798a05fef683ca7ebaf38b1dfd28
ClaraHol <97283088+ClaraHol@users.noreply.github.com>
2022-01-07 15:37:31 +0100

    Add files via upload

--- predictAverage.m
+++ predictAverage.m
@@ -1,20 +1,17 @@
-%% Projekt 2
 function predAv = predictAverage(bredde,lende,simMatrix,filmMatrix)
 %Funktionen forudsiger hvad en person vil rate en film, ud fra
 %gennemsnittet af de brugere hvis ratings er tættest personens.
-
 % Input: Brede og længde af filmMatricen. Matrix med similariteterne og
 % matrix med ratings.
-
 %Output: Matrix med de forudsagte ratings
 
 %Forfattere: Anton Espholm og Clara Hollenbeck
 %Dato: 7-1-2022
 
 sSimMatrix=sort(simMatrix,2,'descend');%Sorterer simMatrix fra størst til mindst
-predAv = zeros(bredde,lende);
+predAv = zeros(lende,bredde);
 
-for i=1:bredde
+for i=1:lende
     vekS=sSimMatrix(i,:); %Laver en vektor ud fra række i i sSimMatrix
     vek = simMatrix(i,:); %Laver en vektor ud fra række i i simMatrix
     tal=vekS(2:4); %Laver en vektor med tallene fra plads 2-4 i vekS
